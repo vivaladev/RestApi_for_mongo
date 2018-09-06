7031af9667d46511871a35fdd4cc947d80cd6b3a
Egor Lebedev <lebedevev2@sovcombank.ru>
2018-09-06 15:27:07 +0400

    added some configure classes

---
+++ src/main/java/com/vivaladev/restApi_for_mongo/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.vivaladev.restApi_for_mongo.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+        return super.requestMappingHandlerAdapter();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+}
---
+++ src/main/java/com/vivaladev/restApi_for_mongo/util/ControllerHelper.java
@@ -0,0 +1,9 @@
+package com.vivaladev.restApi_for_mongo.util;
+
+public class ControllerHelper {
+
+    private static final String OK = "ok";
+    private static final String ERROR = "error";
+
+
+}
