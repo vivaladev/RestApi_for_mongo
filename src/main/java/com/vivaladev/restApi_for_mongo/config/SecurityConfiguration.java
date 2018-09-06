efac4fcc3605c22dc8e0ba89449fe326cc4ee1cc
Egor Lebedev <lebedevev2@sovcombank.ru>
2018-09-06 12:07:03 +0400

    Implemented security configuration

---
+++ src/main/java/com/vivaladev/restApi_for_mongo/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.vivaladev.restApi_for_mongo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/fonts/**", "/images/**", "/js/**",
+//                Disable security for main api request
+                "/viva/1/**", "/error");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
--- src/main/java/com/vivaladev/restApi_for_mongo/entity/DefaultEntity.java
+++ src/main/java/com/vivaladev/restApi_for_mongo/entity/DefaultEntity.java
@@ -1,4 +1,11 @@
 package com.vivaladev.restApi_for_mongo.entity;
 
+
 public class DefaultEntity {
+
+    String helloWorld;
+
+    public DefaultEntity() {
+        this.helloWorld = "Hello Springframework";
+    }
 }
