af11fecc160cf2bde4d2dc983f1b8b51c3260bf9
Egor Lebedev <lebedevev2@sovcombank.ru>
2018-09-06 15:38:37 +0400

    Added new dependensies

--- src/main/java/com/vivaladev/restApi_for_mongo/entity/DefaultEntity.java
+++ src/main/java/com/vivaladev/restApi_for_mongo/entity/DefaultEntity.java
@@ -1,9 +1,15 @@
 package com.vivaladev.restApi_for_mongo.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
 public class DefaultEntity {
 
-    String helloWorld;
+    @Getter
+    @Setter
+    private String helloWorld;
 
     public DefaultEntity() {
         this.helloWorld = "Hello Springframework";
